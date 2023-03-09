9b1e119a8d9e16c7664c61cf5976b5f967516e75
Vlad <bojko.vladislav16@gmail.com>
2023-03-09 14:55:09 +0200

    Release version was made

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-let btn = document.querySelector('.btn');
-let x;
-btn.addEventListener('mouseover', () => btn.style.transform = `translateX(${x ? x = 0 : x = 150}px)`);
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('mouseover', () => {
+  const bodyWidth = document.body.clientWidth;
+  const bodyHeight = document.body.clientHeight;
+  const btnWidth = btn.offsetWidth;
+  const btnHeight = btn.offsetHeight;
+  
+  const randomLeft = Math.floor(Math.random() * (bodyWidth - btnWidth));
+  const randomTop = Math.floor(Math.random() * (bodyHeight - btnHeight));
+  
+  btn.style.left = `${randomLeft}px`;
+  btn.style.top = `${randomTop}px`;
+
+  console.log(bodyHeight);
+});
