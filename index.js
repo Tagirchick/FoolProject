7ec94dd0934a33e02e34ec33b88f018eb433fbf3
Vlad <bojko.vladislav16@gmail.com>
2023-03-09 14:20:50 +0200

    script was updated

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 let btn = document.querySelector('.btn');
-let position;
-btn.addEventListener('mouseover', function () {
-  position ? (position = 0) : (position = 150);
-  btn.style.transform = `translate(${position}px,0px)`;
-  btn.style.transition = 'all 0.3s easy';
-});
+let x;
+btn.addEventListener('mouseover', () => btn.style.transform = `translateX(${x ? x = 0 : x = 150}px)`);
